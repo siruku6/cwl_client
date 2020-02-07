adce2a2ce5b4859dc3c501fd1b4a248fc7f3e4c1
Seiya Sasaki <sirukufarios@gmail.com>
2020-02-08 01:32:51 +0900

    make main.py to load information for log_group_name from yml

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 import boto3
 import datetime
 import pandas as pd
+import yaml
+
+
+YAML_PATH = './cloudwatch_cnf.yml'
 
 def request_logs_from_cloudwatch(start_unix, end_unix):
     client = boto3.client('logs')
-    log_group = '/aws/lambda/scheduled_trade'
+    with open(YAML_PATH, mode='r+') as yaml_data:
+        lambda_function_names = yaml.load(yaml_data)['lambda_function_names']
+
+    log_group = '/aws/lambda/{}'.format(lambda_function_names[0])
     return client.filter_log_events(
         logGroupName=log_group,
-        startTime=start,
-        endTime=end
+        startTime=start_unix,
+        endTime=end_unix
     )
 
 
 if __name__ == '__main__':
     now = datetime.datetime.now()
-    yesterday = now - datetime.timedelta(hours=1)
-    now_unix = int(today.timestamp() * 1000)
+    yesterday = now - datetime.timedelta(hours=24)
+    now_unix = int(now.timestamp() * 1000)
     yesterday_unix = int(yesterday.timestamp() * 1000)
 
-    response = request_logs_from_cloudwatch(start_unix=now_unix, end_unix=yesterday_unix)
+    response = request_logs_from_cloudwatch(start_unix=yesterday_unix, end_unix=now_unix)
     result = pd.DataFrame.from_dict(response['events'])
     # import pdb; pdb.set_trace()
