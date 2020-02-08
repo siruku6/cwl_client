31e884f4905f6ec21ecb979b11df228597d21d2c
Seiya Sasaki <sirukufarios@gmail.com>
2020-02-08 13:06:25 +0900

    add dealt: converting unix-timestamps in the loaded logs into human-readable strings

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import boto3
 import datetime
 import pandas as pd
 import yaml
+import models.interface as i_face
 
 
 YAML_PATH = './cloudwatch_cnf.yml'
 
 def request_logs_from_cloudwatch(start_unix, end_unix):
     client = boto3.client('logs')
-    with open(YAML_PATH, mode='r+') as yaml_data:
-        lambda_function_names = yaml.load(yaml_data)['lambda_function_names']
+    target_names = load_target_names()
 
-    log_group = '/aws/lambda/{}'.format(lambda_function_names[0])
+    log_group = '/aws/lambda/{}'.format(target_names[0])
     return client.filter_log_events(
         logGroupName=log_group,
         startTime=start_unix,
@@ -19,12 +19,42 @@ def request_logs_from_cloudwatch(start_unix, end_unix):
     )
 
 
-if __name__ == '__main__':
-    now = datetime.datetime.now()
-    yesterday = now - datetime.timedelta(hours=24)
-    now_unix = int(now.timestamp() * 1000)
-    yesterday_unix = int(yesterday.timestamp() * 1000)
+def load_target_names():
+    with open(YAML_PATH, mode='r+') as yaml_data:
+        lambda_function_names = yaml.load(yaml_data)['lambda_function_names']
+    return lambda_function_names
 
-    response = request_logs_from_cloudwatch(start_unix=yesterday_unix, end_unix=now_unix)
+
+def convert_unix_column_to_datetime(series):
+    unix_to_datetime = lambda unix: datetime.datetime.fromtimestamp(unix)
+    return (series / 1000).astype(int) \
+                          .map(unix_to_datetime) \
+                          .astype(str)
+
+
+if __name__ == '__main__':
+    def ask_log_period():
+        print('[prompt] 取得したいログの開始日時を入力...')
+        year = i_face.ask_number('  1. 何年？', 9999)
+        month = i_face.ask_number('  2. 何月？', 12)
+        day = i_face.ask_number('  3. 何日？', 31)
+        hour = i_face.ask_number('  4. 何時？', 23)
+        print('[prompt] 取得したいログの期間を入力...')
+        hours = i_face.ask_number('  5. 何時間？', 9999)
+
+        start_datetime = datetime.datetime(year=year, month=month, day=day, hour=hour)
+        end_datetime = start_datetime + datetime.timedelta(hours=hours)
+        start_unix = int(start_datetime.timestamp() * 1000)
+        end_unix = int(end_datetime.timestamp() * 1000)
+
+        return start_unix, end_unix
+
+    start_unix, end_unix = ask_log_period()
+    response = request_logs_from_cloudwatch(start_unix=start_unix, end_unix=end_unix)
     result = pd.DataFrame.from_dict(response['events'])
+
+    result['ingestionTime'] = convert_unix_column_to_datetime(result['ingestionTime'])
+    result['timestamp'] = convert_unix_column_to_datetime(result['timestamp'])
+
     # import pdb; pdb.set_trace()
+    # result.to_csv('./cwl_events.csv')
---
+++ models/interface.py
@@ -0,0 +1,56 @@
+def ask_true_or_false(msg):
+    ''' True か False を選択させる '''
+    while True:
+        print(msg, end='')
+        selection = prompt_inputting_decimal()
+        if selection == 1:
+            return True
+        elif selection == 2:
+            return False
+        else:
+            print('[Trader] please input 1 - 2 ! >д<;')
+
+
+def ask_number(msg, limit):
+    ''' limit以下の数値を選択させる '''
+    while True:
+        print(msg, end='')
+        number = prompt_inputting_decimal()
+        if number > limit:
+            print('[ALERT] 0から{}までの値を入力してください'.format(limit))
+        else:
+            return number
+
+
+def select_from_dict(dictionary, menumsg='選択して下さい'):
+    menu = '[interface] {}'.format(menumsg)
+    dict_len = len(dictionary)
+    for i, (key, val) in enumerate(dictionary.items()):
+        menu = '{menu} [{key}]:{val},'.format(menu=menu, key=key, val=val)
+    menu = menu[0:-1] + ': '
+
+    while True:
+        print(menu, end='')
+        selection = input()
+        if selection in dictionary:
+            return dictionary[selection]
+        else:
+            print('[interface] please input {} - {} ! >д<;'.format(dictionary.keys()[0], dictionary.keys()[-1]))
+
+
+def prompt_inputting_decimal():
+    '''
+    整数を入力させ、int型にして返す
+    Parameters
+    ----------
+    -
+    Returns
+    -------
+    int: decimal
+    '''
+    while True:
+        selection = input()
+        if selection.isdecimal():
+            return int(selection)
+        else:
+            print('Please, input the positive value (integer) ! \nINPUT AGAIN: ', end='')
