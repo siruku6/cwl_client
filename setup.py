63484688319bcc3dfe9f41f79d140f02c660b897
Seiya Sasaki <sirukufarios@gmail.com>
2020-03-09 21:35:19 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+import sys
+
+# sys.path.append('./models')
+# sys.path.append('./tests')
+
+setup(
+    name = 'cwl_client',
+    url='https://github.com/siruku6/cwl_client',
+    version = '0.0.1',
+    description='This is the client for AWS CloudWatch Logs',
+    long_description=read_file('README.md'),
+    long_description_content_type='text/markdown',
+    packages = find_packages(),
+    # test_suite = 'test_interface.suite',
+    author='siruku6',
+    python_requires='>=3.7',
+    install_requires=get_requirements('requirements.txt'),
+    license='MIT',
+)
